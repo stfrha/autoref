c8119fdada554354d90b60990818427cfd4860f7
stfrha <mail@fredrikhoffman.se>
2025-09-07 11:16:08 +0200

    First commit

---
+++ autorefui/Models/ModelFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autorefui
+{
+
+   internal class RelatedFile
+   {
+      #region Declarations
+
+      private bool _referenced;
+      private string _relativePath;
+
+
+      #endregion
+
+      #region Constructor
+
+      public ReFile(bool referenced, string relativePath)
+      {
+         _referenced = referenced;
+         _relativePath = relativePath;
+      }
+
+      #endregion
+
+      #region Properties
+
+      public bool Referenced 
+      { 
+         get { return _referenced; } 
+         set { _referenced = value; }
+      }   
+      public string RelativePath 
+      { 
+         get { return _relativePath; } 
+         set { _relativePath = value; }
+      }
+
+      #endregion
+   }
+
+
+
+   internal class ModelFile
+   {
+      #region Declarations
+
+      private string _path;
+      private ObservableCollection<ModelFile> _childrenFiles;
+      private ObservableCollection<ModelFile> _referencedFiles;
+
+      #endregion
+   }
+}
---
+++ autorefui/ViewModels/TreeViewViewModel.cs
@@ -0,0 +1,99 @@
+﻿using MicroMvvm;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace autorefui
+{
+   [Serializable]
+   public class TreeViewViewModel : MicroMvvm.ViewModelBase
+   {
+      private ObservableCollection<TreeViewViewModel> _children;
+      private TreeViewViewModel _treeParent;
+      protected bool _isSelected;
+      protected bool _isExpanded;
+      private bool _isEnabled;
+      private void _localObject;
+      private MainViewModel _mainVm;
+
+      public TreeViewViewModel(
+        TreeViewViewModel treeParent,
+        MainViewModel mainVm,
+        bool enabled = true)
+      {
+         _treeParent = treeParent;
+         _mainVm = mainVm;
+         _isSelected = false;
+         _isExpanded = false;
+         _isEnabled = enabled;
+      }
+
+
+      public TreeViewViewModel TreeParent
+      {
+         get { return _treeParent; }
+         set
+         {
+            _treeParent = value;
+            OnPropertyChanged("");
+         }
+      }
+
+      public bool IsSelected
+      {
+         get { return _isSelected; }
+         set
+         {
+            if (value != _isSelected)
+            {
+               _isSelected = value;
+               OnPropertyChanged("IsSelected");
+            }
+         }
+      }
+
+      public bool IsExpanded
+      {
+         get { return _isExpanded; }
+         set
+         {
+            if (_isExpanded != value)
+            {
+               if (value && _treeParent != null)
+               {
+                  _treeParent.IsExpanded = true;
+               }
+
+               _isExpanded = value;
+               OnPropertyChanged("IsExpanded");
+            }
+         }
+      }
+
+      public bool IsEnabled
+      {
+         get { return _isEnabled; }
+         set
+         {
+            if (value != _isEnabled)
+            {
+               _isEnabled = value;
+               OnPropertyChanged("IsEnabled");
+            }
+         }
+      }
+
+      public MainViewModel MainVm
+      {
+         get { return _mainVm; }
+         set { _mainVm = value; }
+      }
+   }
+}
+
+
